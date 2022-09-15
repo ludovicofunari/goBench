4efd5818ada390635782b0a821127407e131aff3
Ludovico Funari <ludovico.funari@gmail.com>
2022-09-15 21:26:15 +0200

    Update Docker action

---
+++ example/server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "Hi! I'm goBench! :)\n")
+}
+
+func main() {
+
+	http.HandleFunc("/hello", hello)
+
+	http.ListenAndServe(":8080", nil)
+}
